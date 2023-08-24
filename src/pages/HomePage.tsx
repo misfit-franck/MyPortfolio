0330fd95949a06637982bd3aaba02df86d27af5e
Misfit Franck <log.franckmekoulou@outlook.com>
2023-08-24 10:06:35 +0100

    menu website implemented

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,23 @@
-import { Outlet ,Link } from 'react-router-dom';
+import { Outlet ,Link, NavLink } from 'react-router-dom';
 import '../css/Responsive.css';
+import '../css/Modal.css';
 import SVGMenu from '../assets/menu.svg';
-console.info(SVGMenu);
+import SGVDownload from '../assets/download.svg';
+import { useState } from 'react';
 
 const HomePage = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  }
+
+  if (modal) {
+    document.body.classList.add('active-modal');
+  } else {
+    document.body.classList.remove('active-modal');
+  }
+
   const reloadPage = () => {
     window.location.reload();
   }
@@ -27,12 +41,45 @@ const HomePage = () => {
             <div className='app-header__right-content-menu-icon lang-icon'>
               <h4 className='lang'>Fr.</h4>
             </div>
-            <div className='app-header__right-content-menu-icon menu-icon'>
+            <div className='app-header__right-content-menu-icon menu-icon' onClick={toggleModal}>
               <img src={SVGMenu} alt='menu' className='m-icon' />
             </div>
           </div>
         </header>
       </div>
+      {
+        modal && (
+          <div className='modal'>
+            <div onClick={toggleModal} className='overlay'></div>
+            <div className='modal-content'>
+              <h2 id='menu-title'>Menu</h2>
+              <ul className='overview-menu'>
+                <li><NavLink to={'/'} onClick={toggleModal}>Home Page</NavLink></li>
+                <li><NavLink to={'/about'} onClick={toggleModal}>About Me</NavLink></li>
+                <li><NavLink to={'/portfolio'} onClick={toggleModal}>Portfolio</NavLink></li>
+                <li><NavLink to={'/services'} onClick={toggleModal}>Services</NavLink></li>
+                <li><NavLink to={'/contact'} onClick={toggleModal}>Contact</NavLink></li>
+              </ul>
+              <div className="utiliy-information">
+                <div className="utility-information-box">
+                  <div className="left-content">
+                    <h3>Get My References</h3>
+                    <p>You can download a low file to get my references</p>
+                  </div>
+                  <button type='button' className='outlined-btn'><img src={SGVDownload} alt='download icons'/>Download</button>
+                </div>
+                <div className="utility-information-box">
+                  <div className="left-content">
+                    <h3>Tips</h3>
+                    <p><strong>Please use Firefox for good web experience</strong></p>
+                  </div>
+                </div>
+              </div>
+              <button className='modal-close' onClick={toggleModal}>Close</button>
+            </div>
+          </div>
+        )
+      }
       <Outlet />
     </main>
   );
